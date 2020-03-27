3f3235d4038f540371d1c431ed972a005eb63e33
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-03-27 01:33:19 +0100

    Use apt-get, add update C_PACKAGES and CPP_PACKAGES

--- ubuntu/scripts/install.sh
+++ ubuntu/scripts/install.sh
@@ -31,21 +31,25 @@ python3
 PACKAGES_C="
 libcsfml-dev
 libncurses-dev
+libsdl2-dev
+libsdl2-image-dev
+libsdl2-ttf-dev
 "
 
 PACKAGES_CPP="
+libsfml-dev
 "
 
-cd ${SCRIPTS_PATH}
+cd ${SCRIPTS_PATH} || exit 1
 
-apt update
-apt upgrade -y
-apt install -y software-properties-common
+apt-get update
+apt-get upgrade -y
+apt-get install -y software-properties-common
 
 for PPA in ${PPAS_ALL} ; do
     add-apt-repository -y "ppa:${PPA}"
 done
-for PARAM in $@ ; do
+for PARAM in "$@" ; do
     if [[ "$PARAM" = "c" ]]; then
         for PPA in ${PPAS_C} ; do
             add-apt-repository -y "ppa:${PPA}"
@@ -57,23 +61,23 @@ for PARAM in $@ ; do
         done
     fi
 done
-apt update
+apt-get update
 
-apt install -y ${PACKAGES_ALL}
-apt install -y ${PACKAGES_LANG}
-for PARAM in $@ ; do
+apt-get install -y ${PACKAGES_ALL}
+apt-get install -y ${PACKAGES_LANG}
+for PARAM in "$@" ; do
     if [[ "$PARAM" = "c" ]]; then
-        apt install -y ${PACKAGES_C}
+        apt-get install -y ${PACKAGES_C}
     fi
     if [[ "$PARAM" = "cpp" ]]; then
-        apt install -y ${PACKAGES_CPP}
+        apt-get install -y ${PACKAGES_CPP}
     fi
 done
 
-apt autoremove -y
-apt clean
+apt-get autoremove -y
+apt-get clean
 
-for PARAM in $@ ; do
+for PARAM in "$@" ; do
     if [[ "$PARAM" = "rm" ]]; then
         rm -rf ${SCRIPTS_PATH}
     fi
